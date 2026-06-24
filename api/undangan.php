2fe6a2a643c1383649d25cfbfbb66ee976dbd7b3
riizalfahlevii <rizal_fahlevi80@rocketmail.com>
2026-06-24 11:48:40 +0700

    update notif tele

--- api/undangan.php
+++ api/undangan.php
@@ -1,18 +1,29 @@
 <?php
-    // require "../config.php";
-    require "config.php";
+    header("Access-Control-Allow-Origin: *");
+    require "../config.php";
+    // require "config.php";
 
     try {
         if (!empty($_POST)) {
             $row = R::dispense("guests");
             $row->name = $post_form_name;
             $row->kehadiran = $post_form_presence;
+            switch ($post_form_presence) {
+                case '1': $hadir = "Hadir"; break;
+                case '2': $hadir = "Tidak Hadir"; break;
+                case '3': $hadir = "Mungkin Hadir"; break
+                default: $hadir = "unknown"; break;
+            }
             $row->jumlah = $post_form_count;
             $row->ucapan = $post_form_comment;
             $row->input = date("Y-m-d H:i:s");
             $check = R::store($row);
             if ($check) {
                 response("Terima kasih atas ucapan yang diberikan. Kami mohon doanya agar acara dapat berlangsung dengan lancar tanpa kurang suatu apapun.", true, R::getall("select * from guests order by id desc"));
+                sendUndangan("1638131199", "Ada ucapan dari $post_from_name\n" .
+                "Kehadiran: $hadir\n" .
+                "Jumlah: $post_form_count\n" .
+                "Ucapan: $post_form_comment");
             } else {
                 response("Failed", false);
             }
